c1078397815c132e3b2142336189781ddf3952c9
Mihiri0303 <93081@fhss.sjp.ac.lk>
2021-01-21 23:07:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './app/index';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+ 
+
+ 
